1f1d079aa6e1bc110f79b6e04f9252e90b082ddd
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-31 15:56:29 -0500

    Sed instala: pip install Pillow  para manejar inmagenes en django y se crean las migraciones

--- gestion/models.py
+++ gestion/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 # Create your models here.
 
-class DistinosTuristicos(models.Model):
-    nombreCiudad = models.CharField(max_lenght=100) #campo de texto corto
+class DestinosTuristicos(models.Model):
+    nombreCiudad = models.CharField(max_length=100) #campo de texto corto
     descripcionCiudad = models.TextField()# Campo de texto largo
     imagenCiudad = models.ImageField(upload_to='images/')
-    precioTour = models.DecimalField(max_digits=10, dicimal_place=2)
+    precioTour = models.DecimalField(max_digits=10, decimal_places=2)  # Corregido a decimal_places
     ofertaTour = models.BooleanField(default=False)
 
     def __str__(self):
