8e3409aa00cadb474a66b94bc73063d2087500f0
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-31 16:31:25 -0500

    En la funcion listarDestinos() se lista todos los destinos turísticos.

--- gestion/views.py
+++ gestion/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import DestinosTuristicos# Importa el modelo DestinosTuristicos desde el archivo models.py en la misma aplicación.
 from .forms import DestinosTuristicosForm# Importa el modelo DestinosTuristicosForm desde el archivo forms.py 
 # Create your views here.
+"""Esta vista lista todos los destinos turísticos."""
+def listarDestinos(request):
+    destinos = DestinosTuristicos.objects.all()#para obtener todos los registros del modelo DestinosTuristicos.
+    return render(request,'destinations.html',{'destinos': destinos})
 
