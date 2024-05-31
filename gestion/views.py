a9bf3f29915d7dda53aca3d93ac4175be3e5d56d
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-31 16:22:19 -0500

    Se importan los modelos DestinosTuristicos y DestinosTuristicosForm

--- gestion/views.py
+++ gestion/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import DestinosTuristicos# Importa el modelo DestinosTuristicos desde el archivo models.py en la misma aplicación.
+from .forms import DestinosTuristicosForm# Importa el modelo DestinosTuristicosForm desde el archivo forms.py 
 # Create your views here.
+
