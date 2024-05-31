7f87729ea4628ea920dfe5288b917c1128c6d504
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-31 15:13:34 -0500

    Definimos el modelo DestinosTuristicos en models.py de gestion, nombreCiudad

--- gestion/models.py
+++ gestion/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
 # Create your models here.
+
+class DistinosTuristicos(models.Model):
+
+    nombreCiudad = models.CharField(max_lenght = 100)
+    descripcionCiudad =
+    imagenCiudad =
+    precioTour =
+    ofertaTour =
+
+
+
+
+
