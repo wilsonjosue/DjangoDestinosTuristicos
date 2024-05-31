a93568f16add23cf8bc988cc7419a41891f241a0
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-31 16:10:41 -0500

    Se agregan comentarios descriptivos en gestion/forms.py

--- gestion/forms.py
+++ gestion/forms.py
@@ -1,8 +1,9 @@
 # gestion/forms.py
-from django import forms
-from .models import DestinosTuristicos
+from django import forms #Importa el módulo de formularios de Django.
+from .models import DestinosTuristicos # Importa el modelo DestinosTuristicos desde el archivo models.py en la misma aplicación.
 
-class DestinosTuristicosForm(forms.ModelForm):
+class DestinosTuristicosForm(forms.ModelForm):#Define una clase de formulario basada en un modelo de Django
     class Meta:
-        model = DestinosTuristicos
-        fields = ['nombreCiudad', 'descripcionCiudad', 'imagenCiudad', 'precioTour', 'ofertaTour']
+        model = DestinosTuristicos #Indica que este formulario se basa en el modelo DestinosTuristicos
+        """Especifica una lista de campos del modelo que se deben incluir en el formulario. """
+        fields = ['nombreCiudad', 'descripcionCiudad', 'imagenCiudad', 'precioTour', 'ofertaTour']    
