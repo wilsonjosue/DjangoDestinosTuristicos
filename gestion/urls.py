bc912bd161cfa97e0e58a0dd2ef71fbcf4d24877
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-31 17:23:38 -0500

    Se agregan los comentarios que definen las rutas URL para las vistas CRUD. en gestion/urls.py

--- gestion/urls.py
+++ gestion/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listarDestinos, name='listarDestinos'),
-    path('añadir/', views.añadirDestino, name='añadirDestino'),
-    path('modificar/<int:id>/', views.modificarDestino, name='modificarDestino'),
-    path('eliminar/<int:id>/', views.eliminarDestino, name='eliminarDestino'),
+    path('', views.listarDestinos, name='listarDestinos'),# Ruta, lista los destinos turísticos.
+    path('añadir/', views.añadirDestino, name='añadirDestino'),#Ruta, añade un destino turístico.
+    path('modificar/<int:id>/', views.modificarDestino, name='modificarDestino'),#Ruta, modifica un destino existente
+    path('eliminar/<int:id>/', views.eliminarDestino, name='eliminarDestino'),#Ruta, elimina un destino existente
 ]
