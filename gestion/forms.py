14f800f05e8704d418c4372af09c66eb74d1d7fe
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-31 16:06:49 -0500

    Definimos los metadatos para el formulario, especificando cómo debe comportarse en clase interna Meta

--- gestion/forms.py
+++ gestion/forms.py
@@ -3,4 +3,6 @@ from django import forms
 from .models import DestinosTuristicos
 
 class DestinosTuristicosForm(forms.ModelForm):
-    
+    class Meta:
+        model = DestinosTuristicos
+        fields = ['nombreCiudad', 'descripcionCiudad', 'imagenCiudad', 'precioTour', 'ofertaTour']
