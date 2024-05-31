bd34f052e1a04e5506ed821fefd1f1c87973600c
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-31 17:20:09 -0500

    Se terminan de  configurar las URLs para las vistasDefine las rutas URL para las vistas CRUD. en gestion/urls.py

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,3 +1,10 @@
 # gestion/urls.py
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('', views.listarDestinos, name='listarDestinos'),
+    path('añadir/', views.añadirDestino, name='añadirDestino'),
+    path('modificar/<int:id>/', views.modificarDestino, name='modificarDestino'),
+    path('eliminar/<int:id>/', views.eliminarDestino, name='eliminarDestino'),
+]
