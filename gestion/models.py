c4e21eed653eaf5770c82656c74e87c7d5b7361f
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-31 15:36:51 -0500

    Tratamos de aplicar las migraciones

--- gestion/models.py
+++ gestion/models.py
@@ -2,11 +2,10 @@ from django.db import models
 # Create your models here.
 
 class DistinosTuristicos(models.Model):
-
-    nombreCiudad = models.CharField(max_lenght = 100) #campo de texto corto
+    nombreCiudad = models.CharField(max_lenght=100) #campo de texto corto
     descripcionCiudad = models.TextField()# Campo de texto largo
-    imagenCiudad = models.ImageField(upload_to = 'images/')
-    precioTour = models.DecimalField(max_digits = 10, dicimal_place = 2)
+    imagenCiudad = models.ImageField(upload_to='images/')
+    precioTour = models.DecimalField(max_digits=10, dicimal_place=2)
     ofertaTour = models.BooleanField(default=False)
 
     def __str__(self):
