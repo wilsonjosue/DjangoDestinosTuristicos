259cf15c5deaf486762d21981c20a47b7bf76187
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-31 15:24:59 -0500

    Definimos el campo del modelo precioTour

--- gestion/models.py
+++ gestion/models.py
@@ -6,7 +6,7 @@ class DistinosTuristicos(models.Model):
     nombreCiudad = models.CharField(max_lenght = 100) #campo de texto corto
     descripcionCiudad = models.TextField()# Campo de texto largo
     imagenCiudad = models.ImageField(upload_to = 'images/')
-    precioTour =
+    precioTour = models.DecimalField(max_digits = 10, dicimal_place = 2)
     ofertaTour =
 
 
