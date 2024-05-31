40331017652ec0c1a79062b4ea471ab41e70c519
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-31 15:22:39 -0500

    Definimos el campo del modelo imagenCiudad

--- gestion/models.py
+++ gestion/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 class DistinosTuristicos(models.Model):
 
-    nombreCiudad = models.CharField(max_lenght = 100)
-    descripcionCiudad = models.TextField()
-    imagenCiudad =
+    nombreCiudad = models.CharField(max_lenght = 100) #campo de texto corto
+    descripcionCiudad = models.TextField()# Campo de texto largo
+    imagenCiudad = models.ImageField(upload_to = 'images/')
     precioTour =
     ofertaTour =
 
