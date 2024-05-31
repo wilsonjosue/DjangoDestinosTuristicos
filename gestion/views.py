393a466ebe9abdebc365f7976a2b2fd4ab843728
wilsonjosue <wturpoh@unsa.edu.pe>
2024-05-31 17:10:21 -0500

    Se añaden comentarios para describir su funcionamiento de las funciones implementadas en la carpeta gestion/views.py

--- gestion/views.py
+++ gestion/views.py
@@ -11,31 +11,36 @@ def listarDestinos(request):
 """Permite añadir un nuevo destino turístico."""
 def añadirDestino(request):
     if request.method == 'POST':
+        #Intenta crear un formulario con los datos proporcionados.
         form = DestinosTuristicosForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('listarDestinos')
+        if form.is_valid():#Si es valido el form
+            form.save() #Guarda el nuevo destino  
+            return redirect('listarDestinos')#Redirige a la vista listarDestinos.
     else:
-        form = DestinosTuristicosForm()
+        form = DestinosTuristicosForm()#muestra un formulario vacío.
     return render(request, 'añadirDestino.html', {'form': form}) 
 
 """Permite modificar un destino turístico existente."""
 def modificarDestino(request, id):
+    #Obtiene el destino correspondiente al id proporcionado, o muestra un error 404 si no existe.
     destino = get_object_or_404(DestinosTuristicos, id=id)
+    #Si el método de la solicitud es POST, intenta actualizar el destino con los datos proporcionados.
     if request.method == 'POST':
         form = DestinosTuristicosForm(request.POST, request.FILES, instance=destino)
-        if form.is_valid():
-            form.save()
-            return redirect('listarDestinos')
+        if form.is_valid():#Si es valido el form.
+            form.save() #Guarda los cambio.
+            return redirect('listarDestinos')#Redirige a la vista listarDestinos.
     else:
-        form = DestinosTuristicosForm(instance=destino)
+        #Muestra un formulario con los datos existentes del destino.
+        form = DestinosTuristicosForm(instance=destino)#Renderiza el template añadirDestino.html
     return render(request, 'añadirDestino.html', {'form': form})
 
 """Permite eliminar un destino turístico."""
 def eliminarDestino(request, id):
+    #Obtiene el destino correspondiente al id proporcionado, o muestra un error 404 si no existe.
     destino = get_object_or_404(DestinosTuristicos, id=id)
     if request.method == 'POST':
-        destino.delete()
-        return redirect('listarDestinos')
+        destino.delete()#Elimina el destino 
+        return redirect('listarDestinos')#Redirige a la vista listarDestinos.
     return render(request, 'eliminarDestino.html', {'destino': destino})
 
