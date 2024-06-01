33af382e9283a7f530b1bccb6088bad3d2428844
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-01 13:36:57 -0500

    Se añade una ruta index en gestion/urls.py

--- gestion/urls.py
+++ gestion/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index,name='index' ),# Ruta, 
     path('', views.listarDestinos, name='listarDestinos'),# Ruta, lista los destinos turísticos.
     path('añadir/', views.añadirDestino, name='añadirDestino'),#Ruta, añade un destino turístico.
     path('modificar/<int:id>/', views.modificarDestino, name='modificarDestino'),#Ruta, modifica un destino existente
