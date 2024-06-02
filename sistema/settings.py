77480648ae5bdd18a5517e136d1264e43ba8a0ca
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-02 11:11:12 -0500

    Se configura DATABASES para conectar con nuestra base de datos postgresql en sistema/settings.py

--- sistema/settings.py
+++ sistema/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'sistema.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sistema',
+        'USER': 'postgres',
+        'PASSWORD': '1218',
+        'HOST': 'localhost'
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
