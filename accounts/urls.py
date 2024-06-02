4da3e62f88aa2f2101738b8a5306eb38b2a4509c
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-02 16:28:52 -0500

    Se implementa register.html y se configura en accounts urls.py y views.py para poder ingresar a  resgister.html.El cambio se realizo en account y templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.register,name='register' ),# Ruta, 
+    path('register/', views.register, name='register'),# Ruta, 
 ]
