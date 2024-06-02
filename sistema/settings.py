76a92aa759bfff30c53c7f6d8feea44444b95ba4
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-02 11:48:58 -0500

    Se resuelve el error de travello/apps.py indicando el nuevo combre de la clase

--- sistema/settings.py
+++ sistema/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'travello',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
