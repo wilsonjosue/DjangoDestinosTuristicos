76b419fbe0821ea9661bb08f2c12d83efc105f84
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-02 20:02:19 -0500

    Se configura para mostrar mensajer de error al crear usuario. Se realizo el cambio en accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 # Create your views here.
 
@@ -14,15 +15,18 @@ def register(request):
         
         if password1==password2:
             if User.objects.filter(username=username).exists():
-                print('Username taken')
+                messages.info(request,'Username Taken')
+                return redirect('register')
             elif User.objects.filter(email=email).exists():
-                print('email taken')
+                messages.info(request,'Email Taken')
+                return redirect('register')
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save();
                 print('user created')
         else:
             print('password not matching..')
+            return redirect('register')
         return redirect('/')
     else:
         return render(request,'register.html')
