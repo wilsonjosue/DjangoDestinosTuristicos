8ac559e4d961a73d08e2679e5714a09da2840132
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-02 19:04:43 -0500

    Se soluciona el error de registro The current path, accounts/register/register, didn’t match any of these el problema se encontraba en templates/register.html action={% url register %} method=post.

--- travello/urls.py
+++ travello/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='index' ),# Ruta, 
-    path('', views.listarDestinos, name='listarDestinos'),# Ruta, lista los destinos turísticos.
+    path('listar/', views.listarDestinos, name='listarDestinos'),# Ruta, lista los destinos turísticos.
     path('añadir/', views.añadirDestino, name='añadirDestino'),#Ruta, añade un destino turístico.
     path('modificar/<int:id>/', views.modificarDestino, name='modificarDestino'),#Ruta, modifica un destino existente
     path('eliminar/<int:id>/', views.eliminarDestino, name='eliminarDestino'),#Ruta, elimina un destino existente
