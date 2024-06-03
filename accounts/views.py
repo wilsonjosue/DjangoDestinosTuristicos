f527f295d4085e1cfba9d0403f3403ca7d99ed15
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-02 19:51:54 -0500

    Se soluciona el error en la comparacion de if y elif en views.py. Se realizo el cambio en accounts/views.py

--- accounts/views.py
+++ accounts/views.py
@@ -11,10 +11,18 @@ def register(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         email = request.POST['email']
-
-        user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
-        user.save();
-        print('user created')
+        
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                print('Username taken')
+            elif User.objects.filter(email=email).exists():
+                print('email taken')
+            else:
+                user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save();
+                print('user created')
+        else:
+            print('password not matching..')
         return redirect('/')
     else:
         return render(request,'register.html')
